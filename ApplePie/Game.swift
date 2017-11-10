997d3390eca66caa922e9bf0bd529798cc2aa5f9
Martijn Blauw <martijnblauw@wcw-stud-145-109-14-87.wireless.uva.nl>
2017-11-10 11:53:30 +0100

    Guided Project Apple Pie

---
+++ ApplePie/Game.swift
@@ -0,0 +1,34 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Martijn Blauw on 09-11-17.
+//  Copyright © 2017 Martijn Blauw. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.characters.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word.characters {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
